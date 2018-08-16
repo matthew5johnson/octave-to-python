e5b8ee82aac8323616af69f8f1b25327a2260cfe
matthew5johnson <32151026+matthew5johnson@users.noreply.github.com>
2018-08-16 15:07:02 -0500

    Specified the use of only the second column of x

--- plotData.py
+++ plotData.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def plotData(X, y):
-	plt.scatter(X, y, linestyle='-', color='red', marker="x")
+	plt.scatter(X[:,1], y, linestyle='-', color='red', marker="x")
 	plt.xlabel("Population of city in 10,000s")
 	plt.ylabel("Profit in $10,000s")
 	plt.show()
